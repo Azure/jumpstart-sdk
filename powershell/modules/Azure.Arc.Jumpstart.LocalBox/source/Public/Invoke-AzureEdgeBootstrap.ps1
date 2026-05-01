0985de175ab2316dddd98536a2f60a0ca6b77b01
Jan Egil Ring <janegilring@users.noreply.github.com>
2026-05-01 08:43:32 +0200

    Comment out scheduled task start command
    Comment out the scheduled task start command in the bootstrap script.

--- powershell/modules/Azure.Arc.Jumpstart.LocalBox/source/Public/Invoke-AzureEdgeBootstrap.ps1
+++ powershell/modules/Azure.Arc.Jumpstart.LocalBox/source/Public/Invoke-AzureEdgeBootstrap.ps1
@@ -8,7 +8,7 @@ function Invoke-AzureEdgeBootstrap {
         Write-Host "🛠️ Running bootstrap script on $($node.Hostname)..."
         Invoke-Command -VMName $node.Hostname -Credential $localCred -ScriptBlock {
           # & C:\startupScriptsWrapper.ps1 'C:\BootstrapPackage\bootstrap\content\Bootstrap-Setup.ps1 -Install'
-          Get-ScheduledTask -TaskName ImageCustomizationScheduledTask | Start-ScheduledTask
+          # Get-ScheduledTask -TaskName ImageCustomizationScheduledTask | Start-ScheduledTask
         }
 
         Write-Host "⏳ Waiting for modules and agent on $($node.Hostname)..."
